c6705e1bba0be2f4a18feb28fc6ea7884b481a30
yuanbin-wu <17346682428@163.com>
2019-11-21 12:17:28 +0800

    20191121

--- shiyan3/sort.c
+++ shiyan3/sort.c
@@ -5,10 +5,12 @@ void InsertSort(datatype list[],int n){
 	int compare = 0;
 	int move = 0;
 	for (i = 2; i < n; ++i){
-		if (list[i].data < list[i-1].data,compare++){
-			
+		compare++;
+		if (list[i].data < list[i-1].data){
 			list[0] = list[i];
-			for (j = i-1; list[0].data < list[j].data; --j,compare++){
+			compare++;
+			for (j = i-1; list[0].data < list[j].data; --j){
+				
 				move++;
 				list[j+1] = list[j];
 			}
@@ -27,7 +29,9 @@ void bubbleSort(datatype list[],int n){
 	int move = 0;
 	for (i = 0; i < n; ++i)
 		for (j = 0; j < n-i; ++j){
-			if (list[j].data > list[j+1].data,compare++){
+			compare++;
+			if (list[j].data > list[j+1].data){
+				
 				move++;
 				tmp = list[j].data;
 				list[j].data= list[j+1].data;
@@ -45,17 +49,19 @@ void SelectSort(datatype list[],int n){
 	int move = 0;
 	for (i = 1; i < n; ++i){
 		tmp = i;
-		for (j = i+1; j < n; ++j){
-			if (list[j].data < list[tmp].data,compare++){
+		for (j = i+1; j <n; ++j){
+			compare++;
+			if (list[j].data < list[tmp].data){
 				tmp = j;
 			}
-			if (i != tmp,compare++){
-				move++;
-				list[0] = list[tmp];
-				list[tmp] = list[i];
-				list[i] = list[0];
-			}
+		}	
+		if (i != tmp){
+			move++;
+			list[0] = list[tmp];
+			list[tmp] = list[i];
+			list[i] = list[0];
 		}
+		
 	}
 	printf("关键字比较次数为%d\n",compare);
 	printf("关键字移动次数为%d\n",move );
@@ -65,20 +71,26 @@ void SelectSort(datatype list[],int n){
 
 int Partition(datatype list[],int low, int high){
 
+	//move++;
 	list[0] = list[low];
 	while(low < high){
+		//compare++;
 		while(low < high && list[high].data >= list[0].data)
 			high--;
 		if(low < high){
+			//move++;
 			list[low] = list[high];
 			low++;
 		}
+		//compare++;
 		while(low < high && list[low].data < list[0].data)
 			low++;
 		if(low < high){
+			//move++;
 			list[high] = list[low];
 			high--;
 		}
+		//move++;
 		list[low] = list[0];
 		
 	}
@@ -92,6 +104,8 @@ void QuickSort(datatype list[],int s,int t){
 		QuickSort(list,s,i-1);
 		QuickSort(list,i+1,t);
 	}
+
+
 	return;
 }
 
